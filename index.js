ce2b8824da2aaac27786f19e0bcd8dbbe709e4e8
은정 <141980211+LeeEunJeong0731@users.noreply.github.com>
2023-09-25 14:27:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// const calc = {
+//   add :require("./module/add.js"),
+//   minus : require("./module/minus.js"),
+//   div : require("./module/div.js"),
+//   mult : require("./module/mult.js"),
+// };
+
+// console.log("더하기",calc.add(4, 5));
+// console.log("빼기", calc.minus(4, 5));
+// console.log("나누기", calc.div(4, 5));
+// console.log("더하기", calc.mult(4, 5));
+
+
+const mysachick = require ("./module/leeeunjeong-sachick.js")
+
+console.log (mysachick.minus(3,4));
